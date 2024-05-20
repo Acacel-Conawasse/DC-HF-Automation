be090a6a9fd2b5d938560ca5f1192b54073d7400
Oluwarotimi Malomo <omalom1@students.towson.edu>
2024-05-20 10:06:09 -0400

    Finalized ##Needs review

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pyautogui
+import pyperclip
 import time
+import re
 import subprocess
 import os
 
@@ -14,39 +16,49 @@ def read_and_process_file(input_file, js_template_file, js_modified_file):
             if len(parts) > 1:
                 hyperfind_name = parts[0].strip()
                 schedule_groups = parts[1].strip().split('#')
+                escaped_hyperfind_name = hyperfind_name.replace("'", "\\'") 
 
                 # Modify the JavaScript file
-                js_content[6] = f"const targetCell = document.querySelector('.ui-grid-cell-contents[title=\"{hyperfind_name}\"]');\n"
+                js_content[6] = f"const targetCell = document.querySelector('.ui-grid-cell-contents[title=\"{escaped_hyperfind_name}\"]');\n" 
+                    # Write the modified JavaScript content to the file
                 with open(js_modified_file, 'w') as modified_js:
                     modified_js.writelines(js_content)
 
                 # Execute the GUI automation sequence
                 automate_sequence(schedule_groups)
+                print(f"{hyperfind_name} has been susccessfully created\n")
+                with open('log.txt','a') as log_file:
+                    log_file.write(f"{hyperfind_name} has been susccessfully created\n")
 
 def automate_sequence(schedule_groups):
     # Open console
-    pyautogui.click(5866, 248)
+    pyautogui.click(1570, 1752)
     time.sleep(1)
+    
+
 
     # Copy JS content to clipboard and paste it into the console
     subprocess.run(['clip.exe'], input=open('js_modified.txt', 'r').read().encode('utf-16'), check=True)
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('enter')
     time.sleep(10)
+    pyautogui.rightClick(1703,1397)
+    time.sleep(.25)
+    pyautogui.click(1750,1431)
 
     # Close General Information
-    pyautogui.click(3418, -671)
+    pyautogui.click(79, 389)
     time.sleep(.5)
 
     # Open Scheduling and Schedule Group
-    pyautogui.click(3413, -506)
+    pyautogui.click(74, 633)
     time.sleep(.5)
-    pyautogui.click(3488, -459)
+    pyautogui.click(126, 693)
     time.sleep(.5)
 
     # Process each schedule group
     for group in schedule_groups:
-        pyautogui.click(3768, -575)
+        pyautogui.click(532, 537)
         time.sleep(.5)
         pyautogui.write(group)
         time.sleep(.5)
@@ -56,13 +68,20 @@ def automate_sequence(schedule_groups):
         time.sleep(.5)
 
         # Select and move selected
-        pyautogui.click(3803, -538)
-        time.sleep(.5)
-        pyautogui.click(4708, -532)
-        time.sleep(.5)
+        pyautogui.click(638, 581)
+        time.sleep(1)
+        pyautogui.click(935, 591)
+        time.sleep(1)
+        #Add
+        pyautogui.click(727, 1377)
+        time.sleep(1)
+        #Remove Selected
+        pyautogui.click(944, 729)
+        time.sleep(1)     
+
 
         # Reset search bar
-        pyautogui.click(3768, -575)
+        pyautogui.click(532, 537)
         time.sleep(.5)
         pyautogui.hotkey('ctrl', 'a')
         time.sleep(.5)
@@ -72,18 +91,19 @@ def automate_sequence(schedule_groups):
         time.sleep(.5)
 
     # Apply and save
-    pyautogui.click(5701, 418)
+    pyautogui.click(1367,1787)
     time.sleep(.5)
-    pyautogui.click(5615, 427)
+    pyautogui.click(1235, 1787)
     time.sleep(2)
 
     # Validate the process
-    pyautogui.click(3421, -705, clicks=2)
+    pyautogui.click(87, 328, clicks=2)
     pyautogui.hotkey('ctrl', 'c')
     time.sleep(0.5)  # wait for clipboard
-    result = pyautogui.paste()
+    result = pyperclip.paste()
     if result == "HYPERFINDS":
         print("Processed successfully")
+        
     else:
         print("Terminate program")
         exit()
